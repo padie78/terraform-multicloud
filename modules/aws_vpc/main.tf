1f652bb45ce785c7431a6511318626d26eb12876
Diego Liascovich <padie78@gmail.com>
2026-03-13 20:34:52 +0200

    fix error output

--- modules/aws_vpc/main.tf
+++ modules/aws_vpc/main.tf
@@ -20,23 +20,3 @@ module "vpc_internal" {
     ManagedBy   = "Terraform"
   }
 }
-
-output "vpc_id" { 
-  value = module.vpc_internal.vpc_id 
-}
-
-output "public_subnets" {
-  description = "Necesario para el DMS"
-  value       = module.vpc_internal.public_subnets
-}
-
-output "private_subnets" {
-  description = "Opcional, para control interno"
-  value       = module.vpc_internal.private_subnets
-}
-
-output "database_subnet_group" {
-  description = "Necesario para las RDS"
-  # Nota: El módulo de la comunidad lo exporta con este nombre exacto
-  value       = module.vpc_internal.database_subnet_group_name
-}
